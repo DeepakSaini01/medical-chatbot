f4bd9f4e1e2c6b8d100355217d078e6c9ec3d5c9
DeepakSaini01 <deepaksainii2003@gmail.com>
2025-08-24 16:43:54 +0530

    added and updated prompt file

---
+++ src/prompt.py
@@ -0,0 +1,12 @@
+prompt_template = """
+You are a helpful medical assistant. Using the context below, answer the question concisely in 2-3 sentences.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer briefly:
+"""
+
